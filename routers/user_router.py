90cb54e740f8ff0cf3661538b0d942bac30a72ff
Eli Guy <eliguy343@gmail.com>
2026-01-23 14:00:00 +0200

    register user

--- routers/user_router.py
+++ routers/user_router.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from dto.user_dto import UserCreate, UserLogin
 from models.user_model import UserModel
 from database import SessionLocal
-
+from utils.hash import hash_password, verify_password
 router = APIRouter(
   prefix="/user",
   tags=["user"]
@@ -19,4 +19,15 @@ def get_db():
 
 @router.get('/register')
 def register(user: UserCreate, db: Session = Depends(get_db)):
-  existing_user = db.query(UserModel).filter(UserModel.username == UserModel.username).first()
+  existing_user = db.query(UserModel).filter(UserModel.username == user.username).first()
+  if existing_user:
+    raise HTTPException(status_code=400, detail="Username already exists")
+  existing_user = db.query(UserModel).filter(UserModel.email == user.email).first()
+  if existing_user:
+    raise HTTPException(status_code=400, detail="email already exists")
+  hashed_pw = hash_password(user.password)
+  db_user = UserModel(username=user.username, hashed_password=hashed_pw, email= user.email)
+  db.add(db_user)
+  db.commit()
+  db.refresh(UserModel)
+
