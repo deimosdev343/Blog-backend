7bfde99eb9a51e76df0c4361c8988d1ed0f0202a
eliguy343 <eliguy343@gmail.com>
2026-02-13 19:39:41 +0200

    get user posts

--- routers/post_router.py
+++ routers/post_router.py
@@ -18,6 +18,7 @@ def get_db():
     finally:
         db.close()
 
+
 @router.post("/")
 def create_post(post: PostCreate, db: Session = Depends(get_db), user = Depends(get_current_user)):
     
@@ -47,3 +48,17 @@ def get_posts(
             .all()
     )
     
+@router.get("/{user_id}")
+def get_posts_for_user(
+    user_id: int,
+    skip:int = 0,
+    limit: int = 10,
+    db: Session = Depends(get_db)):
+    return (
+        db.query(Post)
+            .filter(Post.author_id == user_id )
+            .order_by(Post.created_at.desc())
+            .offset(skip)
+            .limit(limit)
+            .all()
+    ) 
