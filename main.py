80c5d811ea6723ba39a6cbd456184a0e86cc5881
eliguy343 <eliguy343@gmail.com>
2026-03-07 10:10:49 +0200

    added router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
-from routers import user_router, post_router, follow_router
+from routers import user_router, post_router, follow_router, vote_router
 from database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
@@ -20,6 +20,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(user_router.router)
 app.include_router(post_router.router)
 app.include_router(follow_router.router)
+app.include_router(vote_router.router)
 
 @app.get("/")
 def root():
