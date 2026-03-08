b5ff70471930a037f7ebb1c5d52925d230b88b5a
eliguy343 <eliguy343@gmail.com>
2026-03-08 23:37:35 +0200

    get post votes

--- routers/vote_router.py
+++ routers/vote_router.py
@@ -51,25 +51,24 @@ def get_post(
   user = Depends(get_current_user_if_logged_in)
 ):
    if(user):   
-    user_id = user["id"]
+    user_id = user["id"] if user else None
+    
     stmt = (
           select(
-              func.sum(
-                  case((PostVote.vote == 1, 1), else_=0)
-              ).label("upvotes"),
-
-              func.sum(
-                  case((PostVote.vote == -1, 1), else_=0)
-              ).label("downvotes"),
-
-              func.max(
-                  case(
-                      (PostVote.user_id == user_id, PostVote.vote),
-                      else_=0
-                  )
-              ).label("user_vote")
+            func.sum(case((PostVote.vote == 1, 1), else_=0)).label("upvotes"),
+            func.sum(case((PostVote.vote == -1, 1), else_=0)).label("downvotes"),
+            func.max(case((PostVote.user_id == user_id, PostVote.vote),else_=0)).label("user_vote")
           )
           .where(PostVote.post_id == post_id)
       )
-
+    result = db.execute(stmt).first()
+    upvotes = result.upvotes or 0
+    downvotes = result.downvotes or 0
+    return {
+      "post_id": post_id,
+      "upvotes": upvotes,
+      "downvotes": downvotes,
+      "score":upvotes - downvotes,
+      "user_votes": result.user_vote or 0
+    }
   
