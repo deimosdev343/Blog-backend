2bf350795d220b428b3ebd8c5034e20c2d6a0451
Eli Guy <eliguy343@gmail.com>
2026-03-20 20:20:39 +0200

    post comment

--- models/post_model.py
+++ models/post_model.py
@@ -24,3 +24,16 @@ class PostVote(Base):
   __table_args__ = (
     UniqueConstraint("user_id", "post_id", name="uq_user_post_vote"),
   )
+  
+class PostComment(Base):
+  __tablename__ = "post_comments"
+  
+  id = Column(Integer, primary_key=True)
+  post_id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
+  author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+  title = Column(String(100), nullable=False)
+  content = Column(Text(300000), nullable=False)
+  username = Column(String(100), nullable= False)
+  user_avatar = Column(String(500), nullable=True)
+  created_at = Column(DateTime, default=func.now())
+  updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
