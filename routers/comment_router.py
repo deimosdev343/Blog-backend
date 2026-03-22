cef432c5b6c0ef39ac79810df6f1eb1d479ec9c3
Eli Guy <eliguy343@gmail.com>
2026-03-22 17:23:48 +0200

    comment fetch

--- routers/comment_router.py
+++ routers/comment_router.py
@@ -1,6 +1,28 @@
 from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from sqlalchemy.orm import Session
-from models.post_model import Post, PostVote
+from models.post_model import PostComment
 from database import SessionLocal
 from utils.auth_scheme import get_current_user, blacklist_token, get_current_user_if_logged_in
 from sqlalchemy import update, insert, delete, select, func, case
+
+router = APIRouter(
+  prefix="/comment"
+);
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+
+@router.get("/{post_id}")
+def get_post_comments(
+  post_id: int,
+  db: Session = Depends(get_db),
+  current_user = Depends(get_current_user)
+):
+  comments = db.query(PostComment).filter(PostComment.post_id == post_id).first()
+  return comments;
+  
+  
